3806fec0d4e8f3281ab3f5e9207413da80c6d493
Yevhenii Shushunov <inegveyn@gmail.com>
2024-06-29 17:54:23 +0300

    update for deploy

--- src/app/modules/home/pages/home/home-hero/home-hero.service.ts
+++ src/app/modules/home/pages/home/home-hero/home-hero.service.ts
@@ -7,10 +7,14 @@ import { Injectable } from '@angular/core'
   providedIn: 'root',
 })
 export class HomeHeroService {
-  private apiUrl = 'https://rm-united24-rebuild-api-public.demo.ukrohost.com/appeal/create'
+  // private apiUrl = 'https://rm-united24-rebuild-api-public.demo.ukrohost.com/appeal/create'
+  private apiUrl = [""]
   constructor(private http: HttpClient) {}
 
-  sendUserEmail(email: string): Promise<string> {
-    return firstValueFrom(this.http.post(`${this.apiUrl}`, email))
+  // sendUserEmail(email: string): Promise<string> {
+  //   return firstValueFrom(this.apiUrl)
+  // }
+  sendUserEmail(email: string): string[] {
+    return (this.apiUrl)
   }
 }
